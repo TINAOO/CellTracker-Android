f577cc5b3a0760a7188ce10b6905a2a633811bab
TINAOO <lyuxi123@yahoo.com>
2023-03-12 20:29:38 -0400

    front end

---
+++ app/src/main/java/com/example/celltracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.celltracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // when the app starts
+        setContentView(R.layout.activity_main);
+    }
+}
